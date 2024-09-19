2e769c8b794f2f89df2476bf4b327fd6a452391d
Samuel Burger <xburger2@fi.muni.cz>
2024-09-19 17:14:42 +0200

    changed api adress

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7075") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.samuelburger.me") });
 builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
 builder.Services.AddScoped<TodosApi>();
 builder.Services.AddAuthorizationCore();
